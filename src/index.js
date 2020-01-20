570b65bd3e1cc484a59a6e3753c87cf32ab40040
Alexander Glushchenko <pistch@yandex-team.ru>
2020-01-20 18:11:03 +0300

    Temp commit (moving to another comp)

--- src/index.js
+++ src/index.js
@@ -16,3 +16,27 @@ const connection2 = new Connection({
   middle: { x: 300, y: 300 },
   end: { x: 150, y: 150 }
 });
+
+const connection3 = new Connection({
+  fieldSelector: '.svg'
+}, {
+  start: { x: 125, y: 70 },
+  middle: { x: 350, y: 300 },
+  end: { x: 155, y: 150 }
+});
+
+const connection4 = new Connection({
+  fieldSelector: '.svg'
+}, {
+  start: { x: 10, y: 70 },
+  middle: { x: 30, y: 300 },
+  end: { x: 10, y: 150 }
+});
+
+const connection5 = new Connection({
+  fieldSelector: '.svg'
+}, {
+  start: { x: 220, y: 70 },
+  middle: { x: 200, y: 300 },
+  end: { x: 250, y: 150 }
+});
